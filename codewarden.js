7a84afb2e2784b606a66d8ac197bb0c69afee6ef
davidk1977 <31257420+davidk1977@users.noreply.github.com>
2023-07-05 14:26:41 +0100

    Update codewarden.js to handle basic auth

--- codewarden.js
+++ codewarden.js
@@ -4,10 +4,12 @@ const github = require('@actions/github');
 
 async function runCodeWarden() {
   try {
+    core.info('Code Warden workflow started');
     const githubToken = core.getInput('github-token');
     const jiraUrl = core.getInput('jira-url');
-    const jiraToken = core.getInput('jira-api-token');
-    const codewardenUrl = `${jiraUrl}/jira/rest/analyze/1.0/pr`; 
+    const jiraUser = core.getInput('jira-user');
+    const jiraPwd = core.getInput('jira-password');
+    const codewardenUrl = `${jiraUrl}/rest/analyze/1.0/pr`; 
 
     const context = github.context;
     const eventName = context.eventName;
@@ -22,19 +24,18 @@ async function runCodeWarden() {
     const title = pullRequest.title;
     const filesUrl = pullRequest.url + '/files';
     const commentsUrl = pullRequest.comments_url;
+    const numCommits = pullRequest.commits;
 
-    // Validate the title format
-    const titleRegex = /^[A-Z]{2,}-\d+/; // [A-Z]{2,}-\d+ format
-    if (!titleRegex.test(title)) {
-      core.setFailed('Invalid title format. The format should be "[A-Z]{2,}-\\d+".')
-      process.exit(1);
-    }
-
+    core.debug('commitsUrl:'+commitsUrl);
+    core.debug('title:'+title);
+    core.debug('filesUrl:'+filesUrl);
+    core.debug('commentsUrl:'+filesUrl);
     
     const codewardenPayload = {
       action: 'review_requested',
       api_token: githubToken,
       pull_request: {
+        commits: numCommits,
         commits_url: commitsUrl,
         title: title,
         files_url: filesUrl,
@@ -42,12 +43,21 @@ async function runCodeWarden() {
       }
     }; 
 
-    const response = await axios.post(codewardenUrl, codewardenPayload, {
+    core.debug('codewardenPayload:'+JSON.stringify(codewardenPayload));
+
+    const config = {
+      auth: {
+        username: jiraUser,
+        password: jiraPwd
+      },
       headers: {
-        Authorization: `Bearer ${jiraToken}`,
         'Content-Type': 'application/json'
       }
-    });
+    };
+
+    core.info('Calling Code Warden Endpoint: '+codewardenUrl);
+    const response = await axios.post(codewardenUrl,codewardenPayload,config);
+
 
     if (response.status === 200) {
       core.info('Pull Request Analyzed by Codewarden. Comment has been added to Pull Request');
@@ -55,9 +65,16 @@ async function runCodeWarden() {
       core.setFailed('Failed to Analyze Pull Request');
     }
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed('UnExpected Error: Code Warden encountered an issue '+error.message);
   }
 }
 
 module.exports = { runCodeWarden };
+// Check if running in GitHub Actions
+if (process.env.GITHUB_ACTIONS === 'true') {
+  runCodeWarden()
+    .catch(error => {
+      core.setFailed('UnExpected Error: Code Warden encountered an issue '+error.message);
+    });
+}
 
